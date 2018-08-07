3a6ea0892c2122f5004a9701bc6f6ef91c6b90b5
Zhao <zhaoc1@l-1-01.cm.cluster>
2018-08-07 13:15:54 -0400

    min contig len as argu

--- contigs_by_taxa.R
+++ contigs_by_taxa.R
@@ -1,35 +1,42 @@
 ## Select contigs based on taxonomic assignments
+## Chunyu Zhao 2018/05/22
 
-library(tidyverse)
+library(dplyr)
+library(readr)
+if (! "taxonomizr" %in% rownames(installed.packages()))
+  install.packages("taxonomizr")
 library(taxonomizr)
 
-## Inputs:
+## input
 taxaNodes <- read.nodes(file.path(snakemake@input[['taxa_nodes']]))
 taxaNames <- read.names(file.path(snakemake@input[['taxa_names']]))
 accessionTaxasql <- file.path(snakemake@input[['taxa_sql']])
 
+## parameters
 db <- snakemake@params[['db']]
 taxaName <- snakemake@params[['taxaName']]
+min_contig_len <- as.numeric(snakemake@params[['min_contig_len']])
 
+## output
 summary_files <- snakemake@input[['summary_files']]
 outdir <- snakemake@params[['outdir']]
 report_fp <- snakemake@output[['report']]
 
-## Parse
+## parse blastn  
 blastn <- do.call(rbind, 
                   lapply(1:length(summary_files), 
                          function(x) read_delim(summary_files[x], delim="\t"))) %>% 
-  filter(length >= 2000) %>%
+  filter(length >= min_contig_len) %>%
   filter_(paste("!is.na(", db, ")", sep="")) %>%
   mutate(taxaID = accessionToTaxa(get(db), accessionTaxasql)) 
 
-## Filter by taxaName, this is slower, yet RefSeq doesn't necessarily update the new assembly 
 blastn <- cbind(blastn, getTaxonomy(blastn$taxaID,taxaNodes,taxaNames))
 
+## filter by taxaName, this is slower, yet RefSeq doesn't necessarily update the new assembly 
 taxaName <- gsub("_", " ", taxaName)
 blastn_taxa <- filter(blastn, grepl(taxaName, species, perl=T, ignore.case = T))
 
-## NOW all we need is to write the contig names of the filtered contigs into separate file
+## write the contig names of the filtered contigs into file per sample
 
 dir.create(outdir, showWarnings = FALSE)
 customWrite <- function(DF, outdir=".") {
